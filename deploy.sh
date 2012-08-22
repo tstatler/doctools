f68971e2bff0595f37a3a6f576d96369e0b219e6
Arthur Evans <aevans@appcelerator.com>
2012-08-21 18:26:52 -0700

    Added options for building with Alloy. Note that only the "debug" build works currently.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,65 @@
+DEBUG_TEMPLATE=template
+PROD_TEMPLATE=template-min
 
-if [ ! "$TI_DOCS" ]
-then
-    if [ "$TI_ROOT" ]
-    then
+progname=$0
+
+usage() {
+    echo "Usage: $progname [options] [debug|prod]"
+    echo ""
+    echo "  Options:"
+    echo "  -o <optional_project> (currently, only alloy is supported)"
+    echo ""
+}
+
+while getopts ":o:" opt; do
+    echo $opt $OPTARG
+    case $opt in 
+        o)
+            if [ $OPTARG == "alloy" ]; then
+                include_alloy="include_alloy"
+            else
+                echo "Unknown optional project, $OPTARG">&2
+                usage
+                exit 1
+            fi
+            ;;
+         \?) 
+             echo "Invalid option: -$OPTARG">&2
+             usage
+             exit 1
+             ;;
+         :)
+             echo "Option -$OPTARG requires an argument." >&2
+             usage
+             exit 1
+             ;;
+    esac
+done
+
+echo "Done with opts"
+
+# Skip the options and move on to the positional parameters
+shift $((OPTIND-1))
+
+echo $@
+
+while [ $1 ]; do
+    if [ $1 == "debug" ]; then
+        debug_build="debug"
+    fi
+    shift
+done
+
+if [ ! "$TI_DOCS" ]; then
+    if [ "$TI_ROOT" ]; then
         TI_DOCS=${TI_ROOT}/titanium_mobile/apidoc
     else
         echo "No doc root \$TI_DOCS and \$TI_ROOT not defined. Exiting."
         exit
     fi
 fi
-if [ ! "$JSDUCK" ]
-then
-    if [ "$TI_ROOT" ]
-    then
+if [ ! "$JSDUCK" ]; then
+    if [ "$TI_ROOT" ]; then
         JSDUCK=${TI_ROOT}/jsduck
     else
         echo "No JSDuck dir \$JSDUCK and \$TI_ROOT not defined. Exiting."
@@ -20,10 +67,8 @@ then
     fi
 fi
 
-if [ ! "$DOCTOOLS" ]
-then
-    if [ "$TI_ROOT" ]
-    then
+if [ ! "$DOCTOOLS" ]; then
+    if [ "$TI_ROOT" ]; then
         DOCTOOLS=${TI_ROOT}/doctools
     else
         echo "No doctools dir \$DOCTOOLS and \$TI_ROOT not defined. Exiting."
@@ -31,11 +76,29 @@ then
     fi
 fi
 
+if [ ! "$ALLOY" ]; then
+    if [ "$TI_ROOT" ]; then
+        ALLOY=${TI_ROOT}/alloy
+    else
+        echo "No alloy dir \$ALLOY and \$TI_ROOT not defined. Exiting."
+        exit
+    fi
+fi
+
+if [ $include_alloy ]; then
+    alloyDirs="${ALLOY}/alloy/builtins ${ALLOY}/alloy/lib"
+fi
+
 python ${TI_DOCS}/docgen.py -f jsduck -o ./build
 python ./guides_parser.py --input "./htmlguides/toc.xml" --output "./build/guides"
-
-compass compile ${JSDUCK}/template/resources/sass
-ruby ${JSDUCK}/bin/jsduck --template ${JSDUCK}/template --config ./jsduck.config
+if [ "$debug_build" ] ; then
+    compass compile ${JSDUCK}/template/resources/sass
+    TEMPLATE=${JSDUCK}/${DEBUG_TEMPLATE}
+else
+    (cd ${JSDUCK}; rake compress)
+    TEMPLATE=${JSDUCK}/${PROD_TEMPLATE}
+fi
+ruby ${JSDUCK}/bin/jsduck --template ${TEMPLATE} --config ./jsduck.config $alloyDirs
 cp -r "./htmlguides/images" "dist/images"
 cp -r "./htmlguides/attachments" "dist/attachments"
 cp -r "./htmlguides/css/common.css" "dist/resources/css/common.css"
