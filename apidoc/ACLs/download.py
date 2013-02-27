081dee8719b85ff20ec73e339ca9ae631663b2f5
Arthur Evans <aevans@appcelerator.com>
2013-02-27 11:45:13 -0800

    Removing unnecessary files.

--- apidoc/ACLs/download.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-import html2text
-baseurl="http://cloud.appcelerator.com/docs/api/v1/acls/"
-for method in [ "create", "update", "delete", "show", "add", "remove", "check" ]:
-    html2text_args =  "html2text.py " +baseurl+method +  " > " + method+".md"
-    print html2text_args
-    subprocess.call(html2text_args, shell=True)
-    subprocess.call([ "./hack.py", method+".md", method+".yml" ])
