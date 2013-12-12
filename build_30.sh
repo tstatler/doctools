efd817dcce0b6cac829d0b79ecf657da7f2f0f43
Ben Hatfield <bhatfield@appcelerator.com>
2013-12-11 21:29:52 -0800

    Streamline doc build script

--- build_30.sh
+++ build_30.sh
@@ -1,28 +1,13 @@
-fail_on_error() {
-    if [ $# -ne 2 ] ; then
-        echo "Wrong number of arguments."
-        exit 1
-    fi
-    if [ $1 -ne 0 ] ; then
-        echo "Error: " $2
-        exit 1
-    fi
-}
+if [ ! $TI_ROOT ]; then
+    echo "Error: \$TI_ROOT not defined!  Exiting..."
+fi
+sh repo_update.sh
 outdir=$TI_ROOT/doctools/dist/titanium/3.0
-cd $TI_ROOT/titanium_mobile
-git checkout 3_2_X
-fail_on_error $? "Could not checkout titanium branch."
-git pull upstream 3_2_X
-fail_on_error $? "Pull failed on titanium branch."
-cd $TI_ROOT/alloy
-git checkout 1_3_X
-fail_on_error $? "Could not checkout alloy branch."
-git pull upstream 1_3_X
-fail_on_error $? "Pull failed on alloy branch."
+rm -rf $outdir
+mkdir -p $outdir
 cd $TI_ROOT/doctools
 rm -rf $outdir
 sh deploy.sh -o alloy -o modules -g htmlguides -d $outdir prod
-fail_on_error $? "Deploy failed."
 rm -rf ~/Sites/titanium/3.0
 rm -rf ~/Sites/titanium/landing/index.html
 cp -rf $outdir ~/Sites/titanium/3.0
