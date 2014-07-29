23a676e2197f8f1f9611a1270b297ec6307718f1
Ben Hatfield <bhatfield@appcelerator.com>
2014-07-29 10:22:01 -0700

    [TIDOC-1059] Fetch

--- repo_update_static.sh
+++ repo_update_static.sh
@@ -39,6 +39,7 @@ function repo_update {
     if [ -d $2 ]; then
         echo "Attempting to update the $1 repo on $3/$4..."
         cd $2
+        git fetch $3
         git checkout $4
         fail_on_error $? "Could not checkout $3/$4 for the $1 repo"
         git pull $3 $4
@@ -52,7 +53,7 @@ repo_update jsduck $JSDUCK_DIR origin master
 repo_update titanium_mobile $TI_DIR origin 3_3_X
 repo_update alloy $ALLOY_DIR origin 1_4_X
 repo_update titanium_modules $MOD_DIR origin master
-repo_update ti.coremotione $CORE_MOTION_DIR origin master
+repo_update ti.coremotion $CORE_MOTION_DIR origin master
 repo_update ti.facebook $FB_DIR origin master
 repo_update ti.map $MAP_DIR origin master
 repo_update ti.nfc $NFC_DIR origin master
